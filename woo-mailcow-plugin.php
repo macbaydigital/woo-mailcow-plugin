12ced9cd10b6ee632a448f3ae1b63c6bed4407f3
macbay <3510329+macbaydigital@users.noreply.github.com>
2025-04-08 11:25:32 +0200

    Update woo-mailcow-plugin.php

--- woo-mailcow-plugin.php
+++ woo-mailcow-plugin.php
@@ -2,8 +2,8 @@
 /*
  * Plugin Name: Woo MailCow Plugin
  * Description: Integriert WooCommerce Subscriptions mit MailCow zur Verwaltung von Mailkonten.
- * Version: 1.0.0
- * Author: Macbay
+ * Version: 1.0.1
+ * Author: Macbay Digital
  * GitHub Plugin URI: https://github.com/macbaydigital/woo-mailcow-plugin/
  */
 
@@ -29,12 +29,14 @@ class Woo_MailCow_Plugin {
         require_once plugin_dir_path(__FILE__) . 'includes/class-api.php';
         require_once plugin_dir_path(__FILE__) . 'includes/class-settings.php';
         require_once plugin_dir_path(__FILE__) . 'includes/class-checkout.php';
+        require_once plugin_dir_path(__FILE__) . 'includes/class-subscription.php';
     }
 
     private function register_components() {
         $api = new Woo_MailCow_API();
         $settings = new Woo_MailCow_Settings();
         $checkout = new Woo_MailCow_Checkout();
+        $subscription = new Woo_MailCow_Subscription();
     }
 
     public function missing_dependencies_notice() {
